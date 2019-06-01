d4373061908d84cf2da367486a5daa5598787240
Evgeny <me@awesomeobserver.me>
2019-06-01 17:14:49 +0300

    Init

---
+++ src/guards.ts
@@ -0,0 +1,67 @@
+import { Nullable, Maybe, NonArray, NonPrimitive, Empty, Bottom } from './types';
+
+export const isBlank = <T>(value: T): value is Nullable<T> => value == null;
+
+export const isPresent = <T>(value: T): value is NonNullable<T> => value != null;
+
+export const isFunction = (value: any): value is Function => typeof value === 'function';
+
+export const isBoolean = (value: any): value is boolean => typeof value === 'boolean';
+
+export const isString = (value: any): value is string => typeof value === 'string';
+
+export const isNumber = (value: any): value is number => typeof value === 'number';
+
+export const isArray = <T>(value: any): value is Array<T> => Array.isArray(value);
+
+/**
+ *
+ * @example
+ *
+ * ```ts
+ * type MyMap = { who: string; age: number }
+ * declare const someObj: MyMap | string | number
+ *
+ * if (isObject(someObj)) {
+ *  // $ExpectType MyMap
+ *  someObj
+ * } else {
+ *  // $ExpectType string | number
+ *  someObj
+ * }
+ * ```
+ */
+export const isObject = <T extends Maybe<{}>>(
+  value: T,
+): value is NonArray<NonPrimitive<NonNullable<T>>> =>
+  value != null && !Array.isArray(value) && typeof value === 'object';
+
+export const isDate = (value: any): value is Date =>
+  value instanceof Date && !isNaN(+value);
+
+export const isPromise = (value: any): value is PromiseLike<any> =>
+  value && typeof value.subscribe !== 'function' && typeof value.then === 'function';
+
+/**
+ *
+ * Checks if string OR array OR object is empty.
+ * If you provide another value to check it will throw an error.
+ */
+export const isEmpty = <T extends string | object>(
+  value: T | Empty,
+): value is Bottom<T> => {
+  if (isString(value) || isArray(value)) {
+    return value.length === 0;
+  }
+
+  if (isObject(value)) {
+    return Object.keys(value).length === 0;
+  }
+
+  throw new Error(
+    `checked value must be type of string | array | object. You provided ${
+      // tslint:disable-next-line:strict-type-predicates
+      value === null ? 'null' : typeof value
+    }`,
+  );
+};
---
+++ src/types.ts
@@ -0,0 +1,320 @@
+/**
+ * Use this type definition instead of `Function` type constructor
+ */
+export type AnyFunction = (...args: any[]) => any;
+
+/**
+ * Simple alias to save you keystrokes when defining JS typed object maps
+ */
+export type StringMap<T> = { [key: string]: T };
+
+/**
+ * Alias for the construct signature that describes a type which can construct objects of the generic type T and whose constructor function accepts an arbitrary number of parameters of any type
+ */
+export type Constructor<T = {}> = new (...args: any[]) => T;
+
+/**
+ * Supplement/Negative to Pick from standard tslib.d.ts
+ */
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+/**
+ * As the name hints, $Diff<A, B> is the type representing the set difference of A and B, i.e. A \ B, where A and B are both object types.
+ */
+export type Diff<T, K> = Pick<T, Exclude<keyof T, keyof K>>;
+
+/**
+ * opposite of standard library `NonNullable`
+ */
+export type Nullable<T> = T extends null | undefined ? T : never;
+
+/**
+ * Get only JS primitive types from a type
+ */
+export type Primitive<T> = T extends object ? never : T;
+
+/**
+ * Get non JS primitive types from a type. Opposite of `Primitive`
+ */
+export type NonPrimitive<T> = T extends object ? T : never;
+
+/**
+ * Maybe types accept the provided type as well as null or undefined
+ */
+export type Maybe<T> = T | null | undefined;
+
+/**
+ * Obtain the return type of a constructor function type within array or object.
+ * > Like native lib.d.ts `InstanceType` but for arrays/tuples or objects
+ *
+ * @example
+ *
+ * ```ts
+ * class Foo { hello = 'world' }
+ * class Moo { world = 'hello' }
+ *
+ * const arr: [typeof Foo, typeof Moo] = [Foo, Moo]
+ * const obj: { foo: typeof Foo, moo: typeof Moo } = { foo: Foo, moo: Moo }
+ *
+ * // $ExpectType [Foo, Moo]
+ * type TestArr = InstanceTypes<typeof arr>
+ *
+ * // $ExpectType {foo: Foo, moo: Moo}
+ * type TestObj = InstanceTypes<typeof obj>
+ * ```
+ */
+export type InstanceTypes<T> = {
+  [P in keyof T]: T[P] extends Constructor<infer U> ? U : never
+};
+
+/**
+ * extracts union type from tuple
+ *
+ * @example
+ *
+ * ```ts
+ * type Tuple = [number, string, boolean]
+ * // $ExpectType number | string | boolean
+ * type Test = UnionFromTuple<Tuple>
+ * ```
+ */
+export type UnionFromTuple<T> = T extends (infer U)[] ? U : never;
+
+/**
+ * Extracts arguments tuple type from a function.
+ * This is useful with React children as a function(render prop) pattern, when implementing HoC
+ *
+ * @example
+ *
+ * ```ts
+ * const funcTestOneArgs = (one: number) => { return }
+ * // $ExpectType [number]
+ * type Test = FunctionArgsTuple<typeof funcTestNoArgs>
+ * ```
+ *
+ * @deprecated
+ * Instead use standard library `Parameters` mapped type
+ */
+export type FunctionArgsTuple<T> = T extends (...args: infer U) => any ? U : never;
+
+/**
+ * Represents the union type of all the value types of the enumerable properties in an object Type T
+ */
+export type Values<T extends object> = T extends { [k: string]: infer V } ? V : never;
+
+/**
+ * Get Proper key types union even from distributed union types.
+ *
+ * `keyof` doesn't work/distribute on union types. This mapped type fixes this issue
+ *
+ * @example
+ *
+ * ```ts
+ * type SomeUnion = { one: number } | { three: string } | { four: boolean }
+ * // $ExpectType 'one' | 'three' | 'four'
+ * type Test = Keys<SomeUnion>
+ * ```
+ */
+export type Keys<T> = T extends any ? keyof T : never;
+
+/**
+ * Get proper known keys from object which contains index type `[key:string]: any`
+ *
+ * @example
+ *
+ * ```ts
+ * type MapWithIndexType = { one: number; two: string; [k: string]: any; }
+ * // $ExpectType 'one' | 'two'
+ * type Test = KnownKeys<MapWithIndexType>
+ * ```
+ */
+export type KnownKeys<T> = {
+  [K in keyof T]: string extends K ? never : number extends K ? never : K
+} extends { [_ in keyof T]: infer U }
+  ? U
+  : never;
+
+/**
+ * Get required only known keys from object which contains index type `[key:string]: any`
+ *
+ * @example
+ *
+ * ```ts
+ * type MapWithIndexType = { one: number; two?: string; [k: string]: any; }
+ * // $ExpectType 'one'
+ * type Test = RequiredKnownKeys<MapWithIndexType>
+ * ```
+ */
+export type RequiredKnownKeys<T> = {
+  [K in keyof T]: {} extends Pick<T, K> ? never : K
+} extends { [_ in keyof T]: infer U }
+  ? U
+  : never;
+
+/**
+ * Get optional only known keys from object which contains index type `[key:string]: any`
+ *
+ * @example
+ *
+ * ```ts
+ * type MapWithIndexType = { one: number; two?: string; [k: string]: any; }
+ * // $ExpectType 'two'
+ * type Test = OptionalKnownKeys<MapWithIndexType>
+ * ```
+ */
+export type OptionalKnownKeys<T> = {
+  [K in keyof T]: string extends K
+    ? never
+    : number extends K
+    ? never
+    : {} extends Pick<T, K>
+    ? K
+    : never
+} extends { [_ in keyof T]: infer U }
+  ? U
+  : never;
+
+/**
+ * Create nominally typed primitives
+ *
+ * Use this mapped typed for creating types for proper nominal type checking.
+ * 👉 kudos to https://michalzalecki.com/nominal-typing-in-typescript/#approach-4-intersection-types-and-brands
+ *
+ * @example
+ *
+ * ```ts
+ * type USD = Brand<number, "USD">
+ * type EUR = Brand<number, "EUR">
+ *
+ * const usd = 10 as USD
+ * const eur = 10 as EUR
+ *
+ * const gross = (net: USD, tax: USD): USD => (net + tax) as USD
+ *
+ * gross(usd, usd); // ok
+ * gross(eur, usd); // Type '"EUR"' is not assignable to type '"USD"'.
+ * ```
+ */
+export type Brand<K, T> = K & { __brand: T };
+
+/**
+ * Pick key-values from Base provided by Condition generic type. Generic can be an union.
+ *
+ * **NOTE:**
+ * It doesn't work for undefined | null values. for that use `PickWithType`
+ *
+ * @example
+ *
+ * ```ts
+ * type Person = { id: number; name: string; lastName: string; address: { street: string; nr: number; } load: () => Promise<Person> }
+ *
+ * // $ExpectType { id: number; name: string; lastName: string; }
+ * type JsonPrimitive = PickWithTypeUnion<Person, number | string>
+ * ```
+ */
+export type PickWithTypeUnion<Base, Condition> = Pick<
+  Base,
+  { [Key in keyof Base]: Base[Key] extends Condition ? Key : never }[keyof Base]
+>;
+
+/**
+ * Pick key-values from Base provided by Condition generic type. Generic needs to be one type from `null | undefined | object | string | number | boolean`
+ *
+ * @example
+ *
+ * ```ts
+ * type Person = { id: number; name: string; lastName: string; address: { street: string; nr: number; } load: () => Promise<Person> }
+ *
+ * // $ExpectType { id: number; }
+ * type JsonPrimitive = PickWithTypeUnion<Person, number>
+ *
+ * // $ExpectType { street: string | null; city: string | null; }
+ * type NullableValues = PickWithType<{ street: string | null; city: string | null; id?: string }, null > \
+ * ```
+ */
+export type PickWithType<
+  Base,
+  Condition extends null | undefined | object | string | number | boolean
+> = Pick<
+  Base,
+  {
+    [Key in keyof Base]: Condition extends Extract<Base[Key], Condition> ? Key : never
+  }[keyof Base]
+>;
+
+type Blank = null | undefined | void;
+
+/**
+ * @private
+ */
+export type NonArray<T> = T extends any[] ? never : T;
+
+/**
+ * Object collects {} and Array<any> so adding both {} and Array<any> is not needed
+ *
+ * @private
+ */
+export type AllowedEmptyCheckTypes = Blank | string | object;
+
+/**
+ * GetEmpty mapped type that will cast any AllowedEmptyCheckTypes to empty equivalent
+ *
+ * @private
+ */
+export type GetEmpty<T extends AllowedEmptyCheckTypes> = T extends Blank
+  ? T
+  : T extends string
+  ? ''
+  : T extends any[]
+  ? Empty.Array
+  : T extends object
+  ? {}
+  : never;
+
+export interface NonEmptyArray<T> extends Array<T> {
+  0: T;
+}
+
+// https://twitter.com/karoljmajewski/status/1037618989801893888?s=20
+export type Empty = Empty.Array | Empty.Object | Empty.String;
+export declare namespace Empty {
+  type String = '';
+  type Array = never[];
+  type Object = Record<string, never>;
+}
+
+export type Bottom<T> = T extends string
+  ? Empty.String
+  : T extends any[]
+  ? Empty.Array
+  : T extends object
+  ? Empty.Object
+  : never;
+
+export interface StringTMap<T> {
+  [key: string]: T;
+}
+export interface NumberTMap<T> {
+  [key: number]: T;
+}
+
+export interface StringAnyMap extends StringTMap<any> {}
+export interface NumberAnyMap extends NumberTMap<any> {}
+
+export interface StringStringMap extends StringTMap<string> {}
+export interface NumberStringMap extends NumberTMap<string> {}
+
+export interface StringNumberMap extends StringTMap<number> {}
+export interface NumberNumberMap extends NumberTMap<number> {}
+
+export interface StringBooleanMap extends StringTMap<boolean> {}
+export interface NumberBooleanMap extends NumberTMap<boolean> {}
+
+// For every properties K of type T, transform it to U
+export declare function mapObject<K extends string, T, U>(
+  obj: Record<K, T>,
+  f: (x: T) => U,
+): Record<K, U>;
+
+const names = { foo: 'hello', bar: 'world', baz: 'bye' };
+const lengths = mapObject(names, s => s.length); // { foo: number, bar: number, baz: number }
